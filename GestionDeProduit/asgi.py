6b9dfa0a97d248f5b4e6a0b6cbfb0decb8a09d6a
Robinson69430 <moncharmont.robinson@icloud.com>
2026-06-01 09:50:21 +0200

    Terminé

--- GestionDeProduit/asgi.py
+++ GestionDeProduit/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for AnalyseContract project.
+ASGI config for GestionDeProduit project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AnalyseContract.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GestionDeProduit.settings')
 
 application = get_asgi_application()
--- GestionDeProduit/wsgi.py
+++ GestionDeProduit/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AnalyseContract project.
+WSGI config for GestionDeProduit project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AnalyseContract.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GestionDeProduit.settings')
 
 application = get_wsgi_application()
